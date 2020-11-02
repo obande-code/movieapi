a7f2bb3399370e0c92355b6511e1c09b321b934c
james <jamesobande2015@gmail.com>
2020-11-02 18:22:57 +0100

    bug: clear the imput field

--- src/component/search/Search.js
+++ src/component/search/Search.js
@@ -5,12 +5,13 @@ import './Search.scss'
 
 const Search = () => {
     const [query, setQuery] = useState('');
-    //create the state for movies, and update that state appropriate
+    //create the state for movies
     const [movies, setMovies] = useState([]);
     
     
     const searchMovies = async (e) => {
         e.preventDefault();
+        setQuery('');
                 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
